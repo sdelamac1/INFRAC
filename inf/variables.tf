099511700a17ed7ab6677fa0c77950b67f0c03aa
Yansete <yanliudai@gmail.com>
2025-09-29 02:57:36 -0500

    fix: se escribió cada atributo en líneas separadas

--- inf/variables.tf
+++ inf/variables.tf
@@ -1,29 +1,80 @@
-variable "region"        { type = string  default = "us-east-1" }
-variable "aws_profile"   { type = string  default = "default" }
-variable "project_name"  { type = string  default = "corpevent" }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "default"
+}
+
+variable "project_name" {
+  type    = string
+  default = "corpevent"
+}
 
 # Networking
-variable "vpc_cidr"      { type = string  default = "10.20.0.0/16" }
-variable "public_a_cidr" { type = string  default = "10.20.0.0/24" }
-variable "public_b_cidr" { type = string  default = "10.20.1.0/24" }
-variable "priv_a_cidr"   { type = string  default = "10.20.10.0/24" }
-variable "priv_b_cidr"   { type = string  default = "10.20.11.0/24" }
+variable "vpc_cidr" {
+  type    = string
+  default = "10.20.0.0/16"
+}
+variable "public_a_cidr" {
+  type    = string
+  default = "10.20.0.0/24"
+}
+variable "public_b_cidr" {
+  type    = string
+  default = "10.20.1.0/24"
+}
+variable "priv_a_cidr" {
+  type    = string
+  default = "10.20.10.0/24"
+}
+variable "priv_b_cidr" {
+  type    = string
+  default = "10.20.11.0/24"
+}
 
 # RDS
-variable "db_engine"     { type = string  default = "mysql" } # o "postgres"
-variable "db_engine_ver" { type = string  default = "8.0" }   # MySQL 8.0
-variable "db_name"       { type = string  default = "corpeventdb" }
-variable "db_username"   { type = string  default = "admin" }
-variable "db_password"   { type = string  sensitive = true default = "changeme123!" }
+variable "db_engine" {
+  type    = string
+  default = "mysql"
+}
+variable "db_engine_ver" {
+  type    = string
+  default = "8.0"
+}
+variable "db_name" {
+  type    = string
+  default = "corpeventdb"
+}
+variable "db_username" {
+  type    = string
+  default = "admin"
+}
+variable "db_password" {
+  type      = string
+  sensitive = true
+  default   = "changeme123!"
+}
 
 # Frontend
-variable "s3_bucket_name" { type = string default = "corpevent-frontend-demo" }
+variable "s3_bucket_name" {
+  type    = string
+  default = "corpevent-frontend-demo"
+}
 
 # SES
-variable "ses_sender_email" { type = string default = "noreply@example.com" }
+variable "ses_sender_email" {
+  type    = string
+  default = "noreply@example.com"
+}
 
 # Tags
 variable "common_tags" {
   type = map(string)
-  default = { project = "corpevent", owner = "student" }
+  default = {
+    project = "corpevent"
+    owner   = "student"
+  }
 }
