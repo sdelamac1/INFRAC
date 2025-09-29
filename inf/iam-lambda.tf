3010fa632d707a0f6135b692dd37037a3f477d3a
Yansete <yanliudai@gmail.com>
2025-09-29 02:52:06 -0500

    fix: corrección en el bloque principals, se escribió cada atributo en lineas separadas

--- inf/iam-lambda.tf
+++ inf/iam-lambda.tf
@@ -1,7 +1,11 @@
 data "aws_iam_policy_document" "lambda_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type = "Service" identifiers = ["lambda.amazonaws.com"] }
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
   }
 }
 
