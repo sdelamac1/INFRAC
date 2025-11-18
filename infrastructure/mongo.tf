0f7ea626bf47c88843af697ee15e142e57e6f1ac
sdelamac1 <sdelamac1@upao.edu.pe>
2025-11-18 02:56:34 +0000

    fix(mongo): Elimino recurso de usuario existente para evitar conflictos

--- infrastructure/mongo.tf
+++ infrastructure/mongo.tf
@@ -1,15 +1,3 @@
-resource "mongodbatlas_database_user" "appuser" {
-  project_id         = var.EXISTING_PROJECT_ID
-  username           = var.db_username
-  password           = var.db_password
-  auth_database_name = "admin"
-
-  roles {
-    role_name     = "readWrite"
-    database_name = "chambea"
-  }
-}
-
 resource "mongodbatlas_project_ip_access_list" "allow_all" {
   project_id = var.EXISTING_PROJECT_ID
   cidr_block = "0.0.0.0/0"
