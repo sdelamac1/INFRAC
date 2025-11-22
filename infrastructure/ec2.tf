6fb56527893f0677061ca97624ffc8ac2b3e57a3
sdelamac1 <sdelamac1@upao.edu.pe>
2025-11-22 19:21:16 +0000

    fix(ec2): Abro todo el trafico de salida (egress) para evitar bloqueos a Mongo/APIs

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -21,29 +21,12 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow outbound HTTP traffic"
+    description = "Allow all outbound traffic "
   }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow outbound tcp traffic"
-  }
-
-  egress {
-  from_port   = 27017
-  to_port     = 27017
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  description = "Allow outbound postgre traffic"
-  }
-
 }
 
 resource "aws_instance" "app1" {
